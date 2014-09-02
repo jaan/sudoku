0a16bfe71a632f9f9336360e891306170c35fa2e
jaan <janardhan@Janardhans-MacBook-Pro.local>
2014-09-02 00:25:33 -0700

    board styles and grouping of squares

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
-(function(undefined){
+(function($,undefined){
 	var boards = {
-			easy:[0,4,0,7,3,0,8,0,0,0,0,7,0,9,0,3,0,5,0,3,6,4,8,0,0,0,0,2,1,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,1,2,0,0,0,0,7,4,2,8,0,6,0,4,0,5,0,7,0,0,0,0,8,0,1,3,0,9,0]
+			easy:[0,4,0,7,3,0,8,0,0,0,0,7,0,9,0,3,0,5,0,3,6,4,8,0,0,0,0,2,1,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,0,1,2,0,0,0,0,7,4,2,8,0,6,0,4,0,5,0,7,0,0,0,0,8,0,1,3,0,9,0],
+			smallSquare:3,
+			largeSquare:9
 		};
 
 	var sudoku = {
@@ -9,29 +11,51 @@
 			var i, j, htmlFragment="", defaultBoardData= this.getBoardData(level), cellDefaultValue, bgClass="", bgIndex=0;
 			htmlFragment ="<div id='board'>"
 			for(i=0;i<81;i++){
-				cellDefaultValue =(defaultBoardData[i] !==0)?defaultBoardData[i]:'.';
+				var defaultBoardCellData = defaultBoardData[i];
 
-				// if(bgIndex > 2){
-				// 	bgIndex = 0;
-				// 	bgClass = (bgClass ==="gray")?"":"gray";
-				// }
-				// bgIndex +=1;
+				cellDefaultValue =(defaultBoardCellData !==0)?defaultBoardCellData:'.';
 
+				//Group the small squares of 3*3 for background color
+				bgClass = (Math.floor((i%boards.largeSquare)/boards.smallSquare)+Math.floor((Math.floor(i/boards.largeSquare))/boards.smallSquare)%2)%2?"":"gray";
+				
+				//This should be moved to templates dust/jade
 				htmlFragment +="<div class='board-"+i+" "+bgClass+"'>"+cellDefaultValue+"</div>";
 
 			}
 			htmlFragment +="</div>";
+			htmlFragment +="<div id='numberSelector'><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span><span>9</span></div>";
+
+			//Timer container
+			htmlFragment +="<div id='timer'>Timer  <span id='minutes'></span> : <span id='seconds'></span></div>";
 			$("#boardWrapper").html(htmlFragment);
 		},
 		getBoardData: function(level){
 			//Returns a board array
 			return boards[level];
 		},
+		startTimer: function(){
+			var startTime = new Date;
+			console.log("Starting Timer");
+			return function(){
+				function displayTimer(){
+					var delta = new Date - startTime; //Time in ms
+					var minutes = ('0'+Math.floor(delta/60/1000)).slice(-2);
+					var seconds = ('0'+Math.floor((delta/1000)%60)).slice(-2);
+
+					$("#minutes").text(minutes);
+					$("#seconds").text(seconds);
+
+				}
+				setInterval(displayTimer, 1000);
+			}
+
+		},
 		init:function(level){
 			this.generateBoardTemplate(level);
+			this.startTimer()();
 		}
 	};
 
 	sudoku.init("easy");
 
-})();
+})(jQuery);
